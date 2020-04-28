4351aca113b71b1143db6c9103ae9dad15ed771b
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2020-04-28 17:56:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import classes from './App.module.css';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
